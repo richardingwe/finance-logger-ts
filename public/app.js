e7950b3b2ae014633254b0fefa89baa7e732fa7e
richardingwe <richardingwe5@gmail.com>
2021-09-19 11:21:13 +0100

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,18 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('paul', 'work on the mario house', 550);
+let invoices = [];
+//form
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toForm = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+class Invoice {
+	client: string;
+	details: string;
+	amount: number;
+
+	constructor(c: string, d: string, a: number) {
+		this.client = c;
+		this.details = d;
+		this.amount = a;
+	}
+
+	format() {
+		return `${this.client} owes £${this.amount} for ${this.details}`;
+	}
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('paul', 'work on the mario house', 550);
+
+let invoices: Invoice[] = [];
+
+//form
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLSelectElement;
