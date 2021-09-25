96ec78622b1ab18560de9250aa222886c82c2842
richardingwe <richardingwe5@gmail.com>
2021-09-25 08:59:31 +0100

    wrap up

--- public/app.js
+++ public/app.js
@@ -8,29 +8,20 @@ const list = new ListTemplate(ul);
 const form = document.querySelector('.new-item-form');
 //input
 const type = document.querySelector('#type');
-const toForm = document.querySelector('#tofrom');
+const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 //add event listener
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(toForm.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// tuples
-let arr = ['ryu', 25, true];
-arr[0] = false;
-arr[1] = 'yoshi';
-arr = [30, false, 'yoshi'];
-let tup = ['ryu', 25, true];
-// tup[0] = false;
-tup[0] = 'ken';
-let student;
-//student = [23564, 'chun-li'];
-student = ['chun-li', 23564];
--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 //input
 const type = document.querySelector('#type') as HTMLInputElement;
-const toForm = document.querySelector('#tofrom') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
@@ -20,26 +20,15 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
 	e.preventDefault();
 
+	let values: [string, string, number];
+	values = [tofrom.value, details.value, amount.valueAsNumber];
+
 	let doc: HasFormatter;
 	if (type.value === 'invoice') {
-		doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+		doc = new Invoice(...values);
 	} else {
-		doc = new Payment(toForm.value, details.value, amount.valueAsNumber);
+		doc = new Payment(...values);
 	}
 
 	list.render(doc, type.value, 'end');
 });
-
-// tuples
-let arr = ['ryu', 25, true];
-arr[0] = false;
-arr[1] = 'yoshi';
-arr = [30, false, 'yoshi'];
-
-let tup: [string, number, boolean] = ['ryu', 25, true];
-// tup[0] = false;
-tup[0] = 'ken';
-
-let student: [string, number];
-//student = [23564, 'chun-li'];
-student = ['chun-li', 23564];
